a430c1b7d216a90972ca8e86d17ba7a2ae8a3533
Thackermahima <81761152+Thackermahima@users.noreply.github.com>
2021-08-19 10:41:38 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function generate(){
+var quotes = {
+
+"- Caitlyn jenner":"I'm just going to go live life. I'm going to go enjoy life. I have nothing left to hide. I am kind of a free person, a free soul.",
+"- John Galsworthy":'“Life calls the tune, we dance.”',
+"-  C.S. Lewis":'“I was not born to be free---I was born to adore and obey.”'
+
+}
+var authors = Object.keys(quotes);
+ var author = authors[Math.floor(Math.random()*authors.length)];
+}
